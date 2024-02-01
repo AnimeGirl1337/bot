46ee2d6b99700c0ebd038f3fc309d031a111d872
AnimeGirl1337 <134223152+AnimeGirl1337@users.noreply.github.com>
2024-02-01 23:45:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import time
+import sched
+import requests
+import telebot
+
+s = sched.scheduler(time.time, time.sleep)
+token = BOT_TOKEN
+bot = telebot.TeleBot(token)
+
+def get_links():
+    response = requests.post('https://anon4ik-get-furry.hf.space/get_images')
+    if response.text:
+        try:
+            return response.json()
+        except json.decoder.JSONDecodeError:
+            print("Ошибка: ответ не является JSON")
+    else:
+        print("Ошибка: пустой ответ")
+    return []
+
+
+def send_message(sc):
+    links = get_links()
+    for link in links:
+        bot.send_message(-4157560547, str(link))
+        bot.send_photo(-1001908844448, str(link))
+    s.enter(3600,1, send_message, (sc,))
+s.enter(3600, 1 , send_message, (s,))
+s.run()
+
+bot.polling(none_stop=True)
