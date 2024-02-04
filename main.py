a4cbe800825036dbf43c15f379371a5ed7ca1ecd
AnimeGirl1337 <134223152+AnimeGirl1337@users.noreply.github.com>
2024-02-04 14:14:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sched
 import requests
 import telebot
 import os
+from telebot.types import InputMediaPhoto
 from back import keep_alive
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
@@ -29,13 +30,18 @@ def send_message(sc):
         links = get_links()
     except Exception as e:
         return str(e)
+    files = []
     for link in links:
+        files.append(InputMediaPhoto(str(link)))
         try:
             bot.send_message(-4157560547, str(link))
             bot.send_photo(-1001908844448, str(link))
-            bot.send_photo(chat_id = -1002095649693, message_thread_id = 4, photo = str(link))
         except Exception as e:
             bot.send_message(-4157560547, str(e))
+    try:
+        bot.send_media_group(chat_id = -1002095649693, message_thread_id = 4, media = files)
+    except Exception as e:
+        bot.send_message(-4157560547, str(e))
     s.enter(3600,1, send_message, (sc,))
 s.enter(3600, 1 , send_message, (s,))
 s.run()
